e0bb9c6a960f4fa7ef1f6da12349935c5b8c732d
Erik Eyler <erikeyler@Eriks-MacBook-Air.local>
2021-10-08 10:17:52 -0400

    created user and event schema

---
+++ models/event.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const Event = new Schema(
+  {
+    date: { type: String, required: true },
+    startTime: { type: String, required: true },
+    endTime: { type: String, required: true },
+    location: { type: String, required: true },
+    title: { type: String, required: true },
+    cost: { type: Decimal128, required: false },
+    details: { type: String, required: true },
+    ageGroup: {
+      type: String,
+      enum: ["All Ages", "18 and over", "16 and over"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("events", Event);
