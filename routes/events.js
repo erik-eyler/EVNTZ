60e3e05dd4a2ba6a6a18b2b1c899ae500618f39d
Erik Eyler <erikeyler@Eriks-MacBook-Air.local>
2021-10-08 11:22:01 -0400

    backend minus authentication up and running

--- routes/events.js
+++ routes/events.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import * as controllers from "../controllers/events.js";
-import restrict from "../helpers/restrict.js";
+// import restrict from "../helpers/restrict.js";
 
 const router = Router();
 
 router.get("/events", controllers.getEvents);
 router.get("/events/:id", controllers.getEvent);
-router.post("/events", restrict, controllers.createEvent);
-router.put("/events/:id", restrict, controllers.updateEvent);
-router.delete("/events/:id", restrict, controllers.deleteEvent);
+router.post("/events", controllers.createEvent);
+router.put("/events/:id", controllers.updateEvent);
+router.delete("/events/:id", controllers.deleteEvent);
 
 export default router;
