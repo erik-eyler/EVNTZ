9e9df4d701bc31af17a6a4674a835d243a29bba4
Erik Eyler <erikeyler@Eriks-MacBook-Air.local>
2021-10-11 13:55:05 -0400

    Events screen (list) react done

--- client/src/screens/Events/Events.jsx
+++ client/src/screens/Events/Events.jsx
@@ -1,8 +1,42 @@
+import Layout from "../../components/Layout/Layout";
+import { useState, useEffect } from "react";
+import EventCard from "../../components/EventCard/EventCard";
+import { getEvents } from "../../services/events";
+
+
+
 const Events = () => {
+  const [events, setEvents] = useState([])
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const events = await getEvents();
+      setEvents(events);
+      setLoading(false);
+    };
+    fetchEvents();
+  }, []);
+
+  const cards = events.map((event, index) =>
+    <EventCard
+    key={index}
+    id={event._id}
+    title={event.title}
+    date={event.date}
+    startTime={event.startTime}
+    endTime={event.endTime}
+    img={event.imgUrl}
+  />)
+
+  if (loading) <div>Loading...</div>;
+
   return (
-    <div>
-      event list
-    </div>
+    <Layout>
+      <div>
+        {cards}
+      </div>
+    </Layout>
   )
 }
 
