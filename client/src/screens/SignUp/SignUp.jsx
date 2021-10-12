f0e57af8f95b72334b0ab41d3156929f5f19dc81
ktbg <ktboates@me.com>
2021-10-12 12:30:54 -0400

    added sign-up form, /services/user.js and user verification on routes in app.js

--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -1,9 +1,132 @@
-const SignUp = () => {
-  return (
-    <div>
-      sign up form
-    </div>
-  )
-}
+import { useState } from "react";
+import "./SignUp.css";
+import { signUp } from "../../services/users";
+import { useHistory } from "react-router-dom";
 
-export default SignUp
+const SignUp = (props) => {
+  const history = useHistory();
+  
+  const [form, setForm] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+    isError: false,
+    errorMsg: "",
+  });
+
+  const handleChange = (event) =>
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+
+  const onSignUp = async (event) => {
+    event.preventDefault();
+    const { setUser } = props;
+    try {
+      const user = await signUp(form);
+      setUser(user);
+      history.push("/");
+    } catch (error) {
+      console.error(error);
+      setForm({
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+        isError: true,
+        errorMsg: "Sign Up Details Invalid",
+      });
+    }
+  };
+
+  const renderError = () => {
+    const toggleForm = form.isError ? "danger" : "";
+    if (form.isError) {
+      return (
+        <button type="submit" className={toggleForm}>
+          {form.errorMsg}
+        </button>
+      );
+    } else {
+      return <button type="submit">Sign Up</button>;
+    }
+  };
+
+  const {
+    firstName,
+    lastName,
+    username,
+    email,
+    password,
+    passwordConfirmation,
+  } = form;
+  console.log(props);
+  return (
+    <div className="form-container">
+      <h3>Sign Up</h3>
+      <form onSubmit={onSignUp}>
+        <label>First Name</label>
+        <input
+          required
+          type="text"
+          name="firstName"
+          value={firstName}
+          placeholder="Enter First Name"
+          onChange={handleChange}
+        />
+        <label>Last Name</label>
+        <input
+          required
+          type="text"
+          name="lastName"
+          value={lastName}
+          placeholder="Enter Last Name"
+          onChange={handleChange}
+        />
+        <label>Username</label>
+        <input
+          required
+          type="text"
+          name="username"
+          value={username}
+          placeholder="Enter username"
+          onChange={handleChange}
+        />
+        <label>Email address</label>
+        <input
+          required
+          type="email"
+          name="email"
+          value={email}
+          placeholder="Enter email"
+          onChange={handleChange}
+        />
+        <label>Password</label>
+        <input
+          required
+          name="password"
+          value={password}
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <label>Password Confirmation</label>
+        <input
+          required
+          name="passwordConfirmation"
+          value={passwordConfirmation}
+          type="password"
+          placeholder="Confirm Password"
+          onChange={handleChange}
+        />
+        {renderError()}
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
