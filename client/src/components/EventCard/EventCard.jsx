92ae1df7fa841336a62e553306b920471e589d01
Erik Eyler <erikeyler@Eriks-MacBook-Air.local>
2021-10-14 09:36:46 -0400

    more events & eventcard styling

--- client/src/components/EventCard/EventCard.jsx
+++ client/src/components/EventCard/EventCard.jsx
@@ -6,11 +6,12 @@ const EventCard = (props) => {
   return (
     <div className="event-card">
       <Link className="card" to={`/events/${props.id}`}>
-        <img src={props.img} alt={props.title} className="card-image"/>
-        <p className="event-card-title">{props.title}</p>
-        <p className="event-card-date">{props.date}</p>
-        <p className="event-card-time">{props.startTime} - {props.endTime}</p>
-        
+        <img src={props.img} alt={props.title} className="event-card-image" />
+        <div className="event-info-container">
+          <p className="event-card-title">{props.title}</p>
+          <p className="event-card-date">{props.date}</p>
+          <p className="event-card-time">{props.startTime} - {props.endTime}</p>
+        </div>
       </Link>
     </div>
   )
