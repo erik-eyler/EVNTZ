16d0634dd834fe8aba4104d6cc96b37dc1c98e93
Breck Zeyer <breckzeyer@Brecks-Air.attlocal.net>
2021-10-14 09:37:50 -0500

    event detail page styled to mvp

--- client/src/screens/EventDetail/EventDetail.jsx
+++ client/src/screens/EventDetail/EventDetail.jsx
@@ -33,36 +33,51 @@ const EventDetail = (props) => {
 
   return (
     <Layout>
-      <div className="event-detail-container">
-        <div className="image-container">
-          <img
-            className="event-detail-img"
-            src={event?.imgUrl}
-            alt={event?.title}
-          />
-        </div>
-        <div className="event-detail-content">
-          <h1 className="content-title">{event?.title}</h1>
-          <div className="sub-container">
-            <h4 className="content-bold">
-              WHEN: <span className="content-regular">{event?.date}</span>
-            </h4>
-            <h4 className="content-bold">
-              TIME: <span className="content-regular">{event?.startTime}</span> -{" "}
-              <span className="content-regular">{event?.endTime}</span>
-            </h4>
+      <div className="event-detail-wrapper">
+        <div className="event-detail-container">
+          <div className="image-container">
+            <img
+              className="event-detail-img"
+              src={event?.imgUrl}
+              alt={event?.title}
+            />
           </div>
-          <div className="sub-container">
-            <div className="content-bold">
-              AGE GROUP: <span className="content-regular">{event?.ageGroup}</span>
+          <div className="event-detail-content">
+            <h1 className="content-title">{event?.title}</h1>
+            <div className="sub-container">
+              <h4 className="content-bold">
+                WHEN: <span className="content-regular">{event?.date}</span>
+              </h4>
+              <h4 className="content-bold">
+                TIME:{" "}
+                <span className="content-regular">{event?.startTime}</span> -{" "}
+                <span className="content-regular">{event?.endTime}</span>
+              </h4>
             </div>
-            <div className="content-bold">COST: <span className="content-regular">{event?.cost}</span></div>
-          </div>
-          <div className="content-bold">LOCATION: <span className="content-regular">{event?.location}</span></div>
-          <div className="content-bold">DETAILS: <span className="content-regular">{event?.details}</span></div>
-          <div className="button-container">
-            <Link to={`/events/${event?._id}/edit`}><button className="update-button">Edit Event</button></Link>
-            <button  className="update-button" onClick={() => handleClick()}>Delete Event</button>
+            <div className="sub-container">
+              <div className="content-bold">
+                AGE GROUP:{" "}
+                <span className="content-regular">{event?.ageGroup}</span>
+              </div>
+              <div className="content-bold">
+                COST: <span className="content-regular">{event?.cost}</span>
+              </div>
+            </div>
+            <div className="content-bold">
+              LOCATION:{" "}
+              <span className="content-regular">{event?.location}</span>
+            </div>
+            <div className="content-bold">
+              DETAILS: <span className="content-regular">{event?.details}</span>
+            </div>
+            <div className="button-container">
+              <Link to={`/events/${event?._id}/edit`}>
+                <button className="update-button">Edit Event</button>
+              </Link>
+              <button className="update-button" onClick={() => handleClick()}>
+                Delete Event
+              </button>
+            </div>
           </div>
         </div>
       </div>
